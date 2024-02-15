398313438c31f49951a7cf382dc3bed31802a436
daffaazhar <daffaazharsda@gmail.com>
2024-02-15 16:55:36 +0700

    feat: make footer responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="relative py-8 bg-glassmorphism border-t border-white">
       <div className="wrapper">
         <div className="absolute bottom-0 right-0 w-52 h-80 bg-[#7c39a3] blur-[175px]"></div>
-        <div className="flex justify-between">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-y-4 md:gap-y-0 md:justify-between">
           <div className="flex gap-x-4 text-white">
             <Link href={""}>Articles</Link>
             <Link href={""}>Projects</Link>
