92fa3f0f86059b96d12050d575e2743db9af91bf
daffaazhar <daffaazharsda@gmail.com>
2024-02-15 15:57:59 +0700

    feat: slicing footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="relative py-8 bg-glassmorphism border-t border-white">
+      <div className="wrapper">
+        <div className="absolute bottom-0 right-0 w-52 h-80 bg-[#7c39a3] blur-[175px]"></div>
+        <div className="flex justify-between">
+          <div className="flex gap-x-4 text-white">
+            <Link href={""}>Articles</Link>
+            <Link href={""}>Projects</Link>
+            <Link href={""}>Contact</Link>
+            <Link href={""}>About</Link>
+          </div>
+          <p className="text-[#c7c7c7]">
+            © 2024 Daffa Azhar. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
